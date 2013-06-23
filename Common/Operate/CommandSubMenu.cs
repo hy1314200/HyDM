0b90b124921c57eefb053faf715308073bff0966
Xingzhe <305827818@qq.com>
2013-06-23 22:16:30 +0800

    漏传

---
+++ Common/Operate/CommandSubMenu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Define;
+using DevExpress.XtraBars;
+
+namespace Common.Operate
+{
+    public class CommandSubMenu : BaseCommand, ICommandEx
+    {
+        public CommandSubMenu()
+        {
+            this.m_Category = "菜单";
+            this.m_Caption = "新菜单";
+
+            this.m_Message = "新菜单";
+            this.m_Tooltip = "从下拉菜单中选择功能";
+        }
+
+        public override void OnClick()
+        {
+        }
+
+        public object ExControl
+        {
+            get { return new BarSubItem(); }
+        }
+    }
+}
